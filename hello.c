b776f465b205c45e89940fb60b18c7eb7b1809a1
Rich Fritsch-Gerdes <rfritsch-gerdes2@huskers.unl.edu>
2022-08-26 03:27:45 +0000

    added exclamation point

--- hello.c
+++ hello.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Hi, my name is Rich Fritsch-Gerdes!\n");
+  printf("Hi, my name is Rich Fritsch-Gerdes!!\n");
   printf("Computer Science Major\n");
 
   return 0;
