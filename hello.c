bb0655fdf545619ddbd204f374df0a4403d8cba4
Rich Fritsch-Gerdes <rfritsch-gerdes2@huskers.unl.edu>
2022-08-26 00:28:32 +0000

    updated file

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ * /
+
+int main(int argc, char **argv) {
+
+  printf("Hi, my name is Rich Fritsch-Gerdes!\n");
+  printf("Computer Science Major\n");
+
+  return 0;
+}
