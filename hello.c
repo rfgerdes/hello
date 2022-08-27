c8f23d90711339efa343a1b16975a4db449e46d8
Matthew Haake <mhaake2@huskers.unl.edu>
2022-08-27 00:19:10 +0000

    Added name and major

--- hello.c
+++ hello.c
@@ -9,6 +9,8 @@ int main(int argc, char **argv) {
 
   printf("Hi, my name is Rich Fritsch-Gerdes!!\n");
   printf("Computer Science Major\n");
-
+  printf("Hi, my name is Matt Haake\n");
+  printf("Computer Science Major\n");
+  
   return 0;
 }
